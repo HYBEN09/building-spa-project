cc877b93307ca0de2e3cc0899bbd7d6e445e3b71
hyebeen byun <hyebeen2658@naver.com>
2023-03-04 20:40:54 +0900

    learn. Link로 페이지들 간에 이동 #3

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,13 @@ import {
   Route,
   RouterProvider,
 } from 'react-router-dom';
-import HomePage from './pages/HomPage';
+import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
 
-// const router = createBrowserRouter([
-//   { path: '/', element: <HomePage /> },
-//   { path: '/products', element: <ProductsPage /> },
-// ]);
-
-const routeDefinitions = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/products" element={<ProductsPage />} />
-  </Route>
-);
-
-const router = createBrowserRouter(routeDefinitions);
+const router = createBrowserRouter([
+  { path: '/', element: <HomePage /> },
+  { path: '/products', element: <ProductsPage /> },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
--- src/pages/HomPage.jsx
+++
@@ -1,5 +0,0 @@
-function HomePage() {
-  return <h1> My Page 🌱 </h1>;
-}
-
-export default HomePage;
