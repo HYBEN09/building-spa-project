d6216d82deae88a63821ff0cb1c99ca16ace16a4
hyebeen byun <hyebeen2658@naver.com>
2023-03-04 23:38:10 +0900

    learn. 프로그램적으로 네비게이션하기 -useNavigate

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,20 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { MainHeader } from '../components/MainHeader';
 
 function HomePage() {
+  const navigate = useNavigate();
+
+  const navigateHandler = () => {
+    navigate('/products');
+  };
+
   return (
     <>
       <h1> My Home Page 🌱 </h1>
       <p>
         Go To <Link to="./Products">the list of products</Link>.
       </p>
+      <button onClick={navigateHandler}>Navigate</button>
     </>
   );
 }
